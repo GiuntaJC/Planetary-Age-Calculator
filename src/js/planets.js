ad0719c8c4278fdc38dea16dd6d2b0e4fad813b5
Joseph Giunta <jgnohack@gmail.com>
2020-11-29 20:30:48 -0800

    refactor all methods slightly, and add new method livedPastExpectancy

--- src/js/planets.js
+++ src/js/planets.js
@@ -5,51 +5,50 @@ export default class planets {
   }
 
   getMercuryAge() {
-    let mercuryAge = this.inputAge/0.24;
-    mercuryAge = parseFloat(mercuryAge.toFixed(2));
+    let mercuryAge = parseFloat((this.inputAge/0.24).toFixed(2));
     return mercuryAge;
   }
 
   getMercuryExpectancy() {
-    let mercuryExpectancy = this.inputExpectancy/0.24;
-    mercuryExpectancy = parseFloat(mercuryExpectancy.toFixed(2));
+    let mercuryExpectancy = parseFloat((this.inputExpectancy/0.24).toFixed(2));
     return mercuryExpectancy;
   }
 
   getVenusAge() {
-    let venusAge = this.inputAge/0.62;
-    venusAge = parseFloat(venusAge.toFixed(2));
+    let venusAge = parseFloat((this.inputAge/0.62).toFixed(2));
     return venusAge;
   }
 
   getVenusExpectancy() {
-    let venusExpectancy = this.inputExpectancy/0.62;
-    venusExpectancy = parseFloat(venusExpectancy.toFixed(2));
+    let venusExpectancy = parseFloat((this.inputExpectancy/0.62).toFixed(2));
     return venusExpectancy;
   }
 
   getMarsAge() {
-    let marsAge = this.inputAge/1.88;
-    marsAge = parseFloat(marsAge.toFixed(2));
+    let marsAge = parseFloat((this.inputAge/1.88).toFixed(2));
     return marsAge;
   }
 
   getMarsExpectancy() {
-    let marsExpectancy = this.inputExpectancy/1.88;
-    marsExpectancy = parseFloat(marsExpectancy.toFixed(2));
+    let marsExpectancy = parseFloat((this.inputExpectancy/1.88).toFixed(2));
     return marsExpectancy;
   }
 
   getJupiterAge() {
-    let jupiterAge = this.inputAge/11.86;
-    jupiterAge = parseFloat(jupiterAge.toFixed(2));
+    let jupiterAge = parseFloat((this.inputAge/11.86).toFixed(2));
     return jupiterAge;
   }
 
   getJupiterExpectancy() {
-    let jupiterExpectancy = this.inputExpectancy/11.86;
-    jupiterExpectancy = parseFloat(jupiterExpectancy.toFixed(2));
+    let jupiterExpectancy = parseFloat((this.inputExpectancy/11.86).toFixed(2));
     return jupiterExpectancy;
+  }
+
+  livedPastExpectancy(planetAge, planetExpectancy) {
+    if(planetAge > planetExpectancy) {
+      const yearsPastExpectancy = parseFloat((planetAge - planetExpectancy).toFixed(2));
+      return yearsPastExpectancy;
+    }
   }
 
 }
